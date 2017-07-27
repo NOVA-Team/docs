76d795d1e8ccb61ef3845dceceab4a1aa9fd45ba
ExE Boss <ExE-Boss@users.noreply.github.com>
2017-07-27 18:31:47 +0200

    Make the homepage chevron always visible
      After some UX testing, I have determined that it might be better when
    the homepage chevron is always visible, plus it doesn’t shift
    the ‘NOVA DOCS’ text into the button.

--- theme/js/base.js
+++ theme/js/base.js
@@ -15,25 +15,3 @@ $('body').scrollspy({
 $("li.disabled a").click(function() {
     event.preventDefault();
 });
-
-$(function() {
-	new MutationObserver(function(mutations) {
-		mutations.forEach(function(mutation) {
-			let prevClassList = mutation.oldValue.split(" ");
-			let target = $(mutation.target);
-			let chevron = $("#nova-chevron-return");
-
-			if (prevClassList.indexOf("collapse") >= 0) {
-				chevron.addClass("expanded expanding");
-			} else if (prevClassList.indexOf("in") >= 0 || (target.hasClass("collapsing") && !chevron.hasClass("expanding"))) {
-				chevron.removeClass("expanded").addClass("collapsing");
-			}
-
-			if (target.hasClass("collapse")) {
-				chevron.removeClass("collapsing");
-			} else if (!target.hasClass("collapse") && !target.hasClass("collapsing")) {
-				chevron.removeClass("expanding");
-			}
-		});
-	}).observe($(".navbar-collapse").get(0), {attributes: true, attributeOldValue: true, attributeFilter: ["class"]});
-});
